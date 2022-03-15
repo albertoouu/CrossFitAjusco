9debc5927c0e7573443d06c8d01fe10c13459ef6
albertoouu <albertoouu@gmail.com>
2022-03-14 22:23:15 -0600

    adding conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-
-import { Navigation } from "./components/NavBar";
-import { Routes, Route } from 'react-router-dom';
+import React, { useState } from "react";
 import Home from "./components/Home.jsx";
+import  {Navigation} from "./components/NavBar.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import app from './components/firebase'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+const auth = getAuth(app)
 
 function App() {
+
+  const[GlobalUser, setGlobalUser] = useState(null);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setGlobalUser(user);
+      console.log(GlobalUser)
+    } else {
+      setGlobalUser(null);
+      console.log(GlobalUser)
+    }
+  });
+
   return (
-    <div>
-      <Navigation /> {/*NavBar */}
-      <Routes>
-        <Route path="/" element={ <Home/> } />
-      </Routes>
-    </div>
+    <>
+      {GlobalUser ? (
+        <Dashboard/>
+      ) : (
+        <Navigation/>
+      )}
+    </>
   );
 }
 
