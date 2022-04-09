875c8095c96343d930880d9c4aedc0b823f3f5be
Elibabah <delias16.hernandez@gmail.com>
2022-04-09 00:10:53 -0500

    butón comunidad post & botones inicar cerrar sesión condicionados

--- src/Community/Post.jsx
+++ src/Community/Post.jsx
@@ -1,47 +1,59 @@
-import "./Post.css";
+import './Post.css';
 //import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
-import React, { useState } from "react";
-import { useAuth } from "../Context/authContext";
-import { PhotoLibrary } from "@mui/icons-material";
-import { collection, addDoc } from "firebase/firestore";
-import { db } from "../firebase";
+import React, { useState } from 'react';
+import { useAuth } from '../Context/authContext';
+import { PhotoLibrary } from '@mui/icons-material';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Post = () => {
   const { user } = useAuth();
+  const [input, setInput] = useState('');
 
-  const [input, setInput] = useState("");
+  const handleChange = (e) => {
+    console.log(e.target.name);
+    setInput(e.target.value);
+    console.log(input);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    //traer objeto para mandarlo a firestore
-    const input = e.target.input.value;
-    console.log(input, user.email);
-    //mandar objeto a firestore
+    const date = new Date().toString();
+    console.log(input, user.email, user.displayName, date);
+    //mandar strings a objeto en firestore
     try {
-      const docRef = await addDoc(collection(db, "Posts"), {
+      const docRef = await addDoc(collection(db, 'Posts'), {
         input: input,
         email: user.email,
+        date: date,
+        author: user.displayName,
       });
-      console.log("Document written with ID: ", docRef.id);
+      console.log('Document written with ID: ', docRef.id);
+      e.target.entry.value = '';
+      setInput('');
     } catch (e) {
-      console.error("Error adding document: ", e);
+      console.error('Error adding document: ', e);
+      e.target.entry.value = '';
+      setInput('');
     }
-    e.target.input.value = "";
   };
 
   // Agregamos un input desde donde el usuario puede escribir sus mensajes
   return (
     <div className="message">
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input name="input" />
+      <form onSubmit={handleSubmit}>
+        <input
+          name="entry"
+          type="text"
+          placeholder="¡Comparte con la comunidad!"
+          onChange={handleChange}
+        />
+        {input === '' ? null : <button>Publicar</button>}
+      </form>
 
-          <button>enviar</button>
-        </form>
-      </div>
       <div className="messageSender_bottom">
         <div className="messageSender_option">
-          <PhotoLibrary style={{ color: "black" }} />
+          <PhotoLibrary style={{ color: 'black' }} />
         </div>
       </div>
     </div>
