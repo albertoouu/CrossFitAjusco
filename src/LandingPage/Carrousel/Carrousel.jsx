2e6b359950616230d0c0d0b50b2b63d967a3e1e3
mon0macal7 <monhdz07@gmail.com>
2022-04-11 23:03:58 -0500

    add new images to carrousel

--- src/LandingPage/Carrousel/Carrousel.jsx
+++ src/LandingPage/Carrousel/Carrousel.jsx
@@ -1,6 +1,16 @@
 import { Carousel } from "react-bootstrap";
 import img1 from "../../assets/lp_imgs/p1.jpg";
 import img2 from "../../assets/lp_imgs/p2.jpg";
+import img3 from "../../assets/lp_imgs/lalostyle.jpg";
+import img4 from "../../assets/lp_imgs/bosque.jpg";
+import img5 from "../../assets/lp_imgs/celebrate.jpg";
+import img6 from "../../assets/lp_imgs/spartan.png";
+import img7 from "../../assets/lp_imgs/tripping.jpg";
+
+{
+  /*Este componente aparece en la página de inicio y contiene las imagenes
+representativas del negocio, con una pequeña descripción */
+}
 
 const Carrousel = () => {
   return (
@@ -12,6 +22,7 @@ const Carrousel = () => {
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
         <img className="d-block w-100" src={img2} alt="Second slide" />
 
@@ -20,15 +31,51 @@ const Carrousel = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/3076516/pexels-photo-3076516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="Third slide"
-        />
 
+      <Carousel.Item>
+        <img className="d-block w-100" src={img3} alt="Third slide" />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Yo seré tu entrenador</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src={img4} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Entrenamiento en el bosque</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src={img5} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Convivio</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src={img6} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Yo seré tu entrenador</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src={img7} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Carrera</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
