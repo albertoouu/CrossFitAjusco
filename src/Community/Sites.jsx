cbad666926b3d58d71269adfc684271b2e7def3a
elibabah <delias16.hernandez@gmail.com>
2022-04-19 13:31:15 -0500

    arch styles community

--- src/Community/Sites.jsx
+++ src/Community/Sites.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Sites = () => {
   return (
     <div style={{ position: 'relative', marginTop: '5%', fontSize: '90%' }}>
-      Sitios de interés
+      Sitios de interés:
     </div>
   );
 };
