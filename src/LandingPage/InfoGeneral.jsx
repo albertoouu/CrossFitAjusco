40c0d9187cf751bc751be242d87017eacb9d2aea
mon0macal7 <monhdz07@gmail.com>
2022-04-22 14:23:57 -0500

    modify carousel sizes

--- src/LandingPage/InfoGeneral.jsx
+++ src/LandingPage/InfoGeneral.jsx
@@ -13,25 +13,25 @@ const InfoGeneral = () => {
   const vision = ` Ser un referendo a nivel local y regional brindando un valor agregado
           a nuestros usuarios y a nuestra comunidad. Lograr que nuestros de
           deportistas hagan una historia llena de logros personales y en equipo. `;
-  const picture = `<img src="https://www.sportlife.es/uploads/s1/75/96/70/8/trail-running.jpeg" alt="" />`;
+  const picture = `"https://www.sportlife.es/uploads/s1/75/96/70/8/trail-running.jpeg"`;
 
   return (
     <div>
       <section className="Info">
         <div>
-          <h3>
-            <span className="Crossft">CrossFT Ajusco</span>
+          <span className="Crossft">CrossFT Ajusco</span>
+          <br />
+          <i>Entrena para ser fuerte, no solo para parecerlo</i>
+          <br />
+          <h2>
+            Más que un centro de entrenamiento, somos una comunidad.
             <br />
-            <i>Entrena para ser fuerte, no solo para parecerlo</i>
-          </h3>
-          <h2>Más que un centro de entrenamiento, somos una comunidad.</h2>
-          <h3>¿Qué es el Crossfit?</h3>
-          <h4>
-            Es entrenamiento funcional realizado a alta intensidad. Se adapta a
-            todos los niveles y está dirigido a cualquier persona.
-          </h4>
+            ¿Qué es el Crossfit? Es entrenamiento funcional realizado a alta
+            intensidad. Se adapta a todos los niveles y está dirigido a
+            cualquier persona.
+          </h2>
         </div>
-        <CardMedia img={picture} />
+        <CardMedia image={picture} />
         <CardActionArea title={`Misión`} content={mision} />
         <CardActionArea title={`Visión`} content={vision} />
       </section>
